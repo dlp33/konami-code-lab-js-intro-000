53ec01049bd2a479b586a87b5842439b1cb932c8
Darren Paddock <darrenleepaddock@gmail.com>
2018-02-14 17:35:28 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,21 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
+var index = 0;
 
+document.addEventListener('keydown', init);
+
+function init(e) {
+
+  var keyPressed = e.which;
+
+  if(e.which === code[index]) {
+    index++;
+
+    if(index === 10) {
+      console.log('You win!');
+      index = 0;
+    }  
+  } else {
+    index = 0;
+  }
 }
