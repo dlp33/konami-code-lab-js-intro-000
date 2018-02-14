96d18046b9db2416a8172bb01974841e263def18
Darren Paddock <darrenleepaddock@gmail.com>
2018-02-14 19:19:42 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,6 +2,8 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 var index = 0;
 
+document.body.addEventListener('keydown', this.init.bind(this));
+
 function init(e) {
 
   console.log(e);
@@ -20,4 +22,4 @@ function init(e) {
   }
 }
 
-document.body.addEventListener('keydown', init);
+
